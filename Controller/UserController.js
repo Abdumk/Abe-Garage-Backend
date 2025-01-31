3b9884842115ce5cfacd5e4d4eb1f80ba64a531a
Abdurahman Mohammed <157899943+Abdumk@users.noreply.github.com>
2025-01-31 23:31:44 +0300

    Add getUsers fun to UserController

--- Controller/UserController.js
+++ Controller/UserController.js
@@ -1,6 +1,19 @@
 // these component used to "for big project there is MVC...the arctict folder structure module(database),view(front end or cliant),controller" found in back-end and communicate with database
 
-      
-
-//import dbconfig
+// Import dbconfig
 const dbconnection = require("../db/dbConfig");
+
+// Example function to get users
+function getUsers(req, res) {
+  dbconnection.query('SELECT * FROM users', (error, results) => {
+    if (error) {
+      return res.status(500).json({ error: error.message });
+    }
+    res.status(200).json(results);
+  });
+}
+
+// Export the function to be used in routes
+module.exports = {
+  getUsers
+};
